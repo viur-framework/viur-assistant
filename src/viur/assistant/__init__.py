44542d5e82efc969d6618ceda607bf2674890d08
Sven Eberth <mail@sveneberth.de>
2025-05-27 19:08:08 +0200

    feat: Add config

--- src/viur/assistant/__init__.py
+++ src/viur/assistant/__init__.py
@@ -7,9 +7,11 @@ AI-based assistance module plugin for ViUR
 from .bones.image import ImageBone, ImageBoneRelSkel
 from .modules.assistant import Assistant
 from .skeletons.assistant import AssistantSkel
+from .config import CONFIG
 
 __all__ = [
     "Assistant",
+    "CONFIG",
     "ImageBone",
     "ImageBoneRelSkel",
 ]
---
+++ src/viur/assistant/config.py
@@ -0,0 +1,22 @@
+import typing as t
+
+from viur.core.config import ConfigType
+
+
+class AssistantConfig(ConfigType):
+    """Configuration for viur-assistant plugin."""
+
+    api_openai_key: str = None
+    """API Key for OpenAi"""
+
+    api_anthropic_key: str = None
+    """API Key for Anthropic"""
+
+
+CONFIG: t.Final[AssistantConfig] = AssistantConfig(
+    strict_mode=True,
+)
+"""
+The viur-assistent config instance.
+"""
+

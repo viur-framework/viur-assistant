5912646fd527aceddadcca40dd036e80e66da549
Sven Eberth <mail@sveneberth.de>
2025-05-27 18:33:18 +0200

    chore: Add version.py

---
+++ src/viur/assistant/version.py
@@ -0,0 +1,8 @@
+# This is the place where the viur-assistant version number is defined;
+# For pre-releases, postfix with ".betaN" or ".rcN" where `N` is an incremented number for each pre-release.
+# This will mark it as a pre-release as well on PyPI.
+
+__version__ = "0.0.0"
+
+assert __version__.count(".") >= 2 and "".join(__version__.split(".", 3)[:3]).isdigit(), \
+    "Semantic __version__ expected!"
